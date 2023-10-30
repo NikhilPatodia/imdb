0d77cd537d847fd39db22c644647895a884ab784
NikhilPatodia <npatodia085@gmail.com>
2023-10-30 20:55:05 +0530

    chnge some values

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 export const dynamic = "force-dynamic";
 import Results from "@/components/Results";
 
-
+const API_KEY = process.env.API_KEY;
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || "fetchTrending"
-  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTrending"?'trending/all/week':'movie/top_rated'}?api_key=${process.env.API_KEY}&language=en-US&page=1`,{next:{revalidate:10000}})
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTrending"?'trending/all/week':'movie/top_rated'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10000}})
   const data = await res.json();
   const results = data.results;
   
